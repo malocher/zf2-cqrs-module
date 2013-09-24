97c568e637306f51267ee2af0af3b0c10f86ea09
Codeliner <kontakt@codeliner.ws>
2013-09-24 19:48:54 +0200

    Initial commit

---
+++ zf2-cqrs-module/src/CqrsModule/Module.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of Module
+ * 
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ * @copyright (c) 2013, Alexander Miertsch
+ */
+namespace CqrsModule;
+
+class Module
+{
+    
+}
